4e207e6992f91daa05a7bf306db6b94ae54cc94f
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2024-01-01 03:24:55 +1100

    Update syscall functions to use new register names.

--- Dream-Machine/src/syscalls.rs
+++ Dream-Machine/src/syscalls.rs
@@ -13,17 +13,17 @@ pub enum Syscall {
 }
 
 pub fn syscall0(vm: &mut VM) {
-    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.sri) };
+    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.rsi) };
     match syscall {
         _ => panic!("Invalid syscall0: {syscall:?}"),
     }
 }
 
 pub fn syscall1(vm: &mut VM) {
-    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.sri) };
+    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.rsi) };
     match syscall {
         Syscall::Close => {
-            let fid = vm.reg.sr[0] as sys::FileID;
+            let fid = vm.reg.rs[0] as sys::FileID;
             sys::io::close(fid);
         }
         _ => panic!("Unvalid syscall1: {syscall:?}"),
@@ -31,60 +31,60 @@ pub fn syscall1(vm: &mut VM) {
 }
 
 pub fn syscall2(vm: &mut VM) {
-    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.sri) };
+    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.rsi) };
     match syscall {
         _ => panic!("Invalid syscall2: {syscall:?}"),
     }
 }
 
 pub fn syscall3(vm: &mut VM) {
-    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.sri) };
+    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.rsi) };
     match syscall {
         Syscall::Read => {
-            let fid = vm.reg.sr[0] as sys::FileID;
+            let fid = vm.reg.rs[0] as sys::FileID;
             let buf = unsafe {
-                std::slice::from_raw_parts_mut(vm.reg.sr[1] as *mut u8, vm.reg.sr[2] as usize)
+                std::slice::from_raw_parts_mut(vm.reg.rs[1] as *mut u8, vm.reg.rs[2] as usize)
             };
-            vm.reg.srr = sys::io::read(fid, buf);
+            vm.reg.rsr = sys::io::read(fid, buf);
         }
         Syscall::Write => {
-            let fid = vm.reg.sr[0] as sys::FileID;
+            let fid = vm.reg.rs[0] as sys::FileID;
             let bytes_to_write = unsafe {
-                std::slice::from_raw_parts(vm.reg.sr[1] as *const u8, vm.reg.sr[2] as usize)
+                std::slice::from_raw_parts(vm.reg.rs[1] as *const u8, vm.reg.rs[2] as usize)
             };
             sys::io::write(fid, bytes_to_write);
         }
         Syscall::Open => {
-            let path_ptr = vm.reg.sr[0] as *const u8;
-            let path_len = vm.reg.sr[1] as usize;
-            let flags = unsafe { std::mem::transmute(vm.reg.sr[2]) };
+            let path_ptr = vm.reg.rs[0] as *const u8;
+            let path_len = vm.reg.rs[1] as usize;
+            let flags = unsafe { std::mem::transmute(vm.reg.rs[2]) };
 
             let path_slice = unsafe { std::slice::from_raw_parts(path_ptr, path_len) };
             let path =
                 std::str::from_utf8(path_slice).expect("Non UTF8 data provided for open syscall.");
 
-            vm.reg.srr = sys::io::open(path, flags);
+            vm.reg.rsr = sys::io::open(path, flags);
         }
         _ => panic!("Invalid syscall3: {syscall:?}"),
     }
 }
 
 pub fn syscall4(vm: &mut VM) {
-    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.sri) };
+    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.rsi) };
     match syscall {
         _ => panic!("Invalid syscall0: {syscall:?}"),
     }
 }
 
 pub fn syscall5(vm: &mut VM) {
-    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.sri) };
+    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.rsi) };
     match syscall {
         _ => panic!("Invalid syscall5: {syscall:?}"),
     }
 }
 
 pub fn syscall6(vm: &mut VM) {
-    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.sri) };
+    let syscall: Syscall = unsafe { std::mem::transmute(vm.reg.rsi) };
     match syscall {
         _ => panic!("Invalid syscall6: {syscall:?}"),
     }
